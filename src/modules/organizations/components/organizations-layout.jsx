ff899c9cc7c234e2d8537ab51b7106f02d9d3b2d
mcbouslog <mcbouslog@gmail.com>
2017-01-26 16:39:52 -0600

    init orgs and org views updated

--- src/modules/organizations/components/organizations-layout.jsx
+++ src/modules/organizations/components/organizations-layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
-import OrganizationActionLink from './organization-action-link';
+// import { Link } from 'react-router';
+// import OrganizationActionLink from './organization-action-link';
 
 /* eslint-disable react/prop-types */
 // i really don't wanna figure out how to give valid propTypes for builtins like children
-const OrganizationsLayout = ({ children, navItems, organization, organizations }) => {
+const OrganizationsLayout = ({ children, organization, organizations }) => {
 /* eslint-enable */
 
   // inject organization into children
@@ -12,38 +12,39 @@ const OrganizationsLayout = ({ children, navItems, organization, organizations }
     React.cloneElement(child, { organization, organizations }),
   );
 
-  const org = organization || {};
+  // const org = organization || {};
 
   return (
-    <div>
-      <aside>
-        <nav>
-          <h4>Navigation</h4>
-          <ul>
-            <li><Link to="">Home</Link></li>
-          </ul>
-          <h4>Organizations</h4>
-          <ul className="organizationsLinks">
-            <li><Link to="/organizations">Organizations List</Link></li>
-            {navItems.map(item =>
-              <OrganizationActionLink key={item.to} to={item.to} id={org.id} text={item.label} />,
-            )}
-          </ul>
-        </nav>
-      </aside>
-
-      <section>{wrappedChildren}</section>
-    </div>
+    <section>{wrappedChildren}</section>
   );
 };
 
-OrganizationsLayout.defaultProps = {
-  navItems: [
-    { to: '', label: 'Edit' },
-    { to: 'about', label: 'About' },
-    { to: 'collaborators', label: 'Collaborators' },
-    { to: 'visibility', label: 'Visibility' },
-  ],
-};
-
 export default OrganizationsLayout;
+
+// removed OrganizationsLayout prop - navItems,
+
+// OrganizationsLayout.defaultProps = {
+//   navItems: [
+//     { to: '', label: 'Edit' },
+//     { to: 'about', label: 'About' },
+//     { to: 'collaborators', label: 'Collaborators' },
+//     { to: 'visibility', label: 'Visibility' },
+//   ],
+// };
+
+
+// <aside>
+//   <nav>
+//     <h4>Navigation</h4>
+//     <ul>
+//       <li><Link to="">Home</Link></li>
+//     </ul>
+//     <h4>Organizations</h4>
+//     <ul className="organizationsLinks">
+//       <li><Link to="/organizations">Organizations List</Link></li>
+//       {navItems.map(item =>
+//         <OrganizationActionLink key={item.to} to={item.to} id={org.id} text={item.label} />,
+//       )}
+//     </ul>
+//   </nav>
+// </aside>
